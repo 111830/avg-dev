275330c3a09ba7347074b66556d5ca6ab1550005
Renis Ndregjoni <ndregjonirenis@gmail.com>
2025-12-30 12:51:07 +0100

    more c stuff

---
+++ project/c/music-tracks/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+char tracks[][80] = {
+    "I left my heart in Harvard Med School",
+    "Newark, Newark - a wonderful town",
+    "Dancing with a Dork",
+    "From here to maternity",
+    "The girl from Iwo Jima",
+};
+
+int main(int argc, char *argv[])
+{
+    // Give a tracks list
+    // Ask the user which track she is looking for
+    // Get the input and use it to search through all tracket
+    // -> loops?
+    // Dispsplay any matches. (when input exist occurs within the track string)
+    // -> hint : strstr() mayby?
+
+    char input[80];
+
+    printf("What is your song? \n");
+    scanf("%s", input);
+
+
+    for(int i = 0; i < 5; i++){
+        char* curr_tracks = tracks[i];
+
+        // Use strstr() to check if input accurs in curr_tracks
+        char* occurrence = strstr(curr_tracks, input);
+
+        if(occurrence != NULL){
+            printf("%s\n", curr_tracks);
+        }
+    }
+
+    return 0;
+}
