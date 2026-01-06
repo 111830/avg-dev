eac95e22956b62d883337c1f1880ef04f2d4909b
Renis Ndregjoni <ndregjonirenis@gmail.com>
2026-01-06 13:48:49 +0100

    More struct

---
+++ project/c/structs-test-3/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct {
+    const char* name;
+    const char* species;
+    int age;
+} turtle;
+
+void happy_birthday(turtle* t){
+    t->age = t->age + 1; // t-> age is the same as (*t).age
+    printf("Happy Birthday %s! You are now %i years old!\n", t->name, t->age);
+}
+
+int main(int argc, char* argv[]){
+    turtle myrtle = {"Myrtle", "Leatherback sea turtle", 99};
+    happy_birthday(&myrtle); // &myrtle -> turtle*
+    printf("%s's age is now %i\n", myrtle.name, myrtle.age);
+
+    return 0;
+}
