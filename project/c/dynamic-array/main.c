f10426f31d180ac3c4de20f623b626d3e8d5a974
Renis Ndregjoni <ndregjonirenis@gmail.com>
2026-02-01 14:59:37 +0100

    Dynamic Array

--- project/c/dynamic-array/main.c
+++ project/c/dynamic-array/main.c
@@ -18,11 +18,48 @@ void initialize_array(DynamicArray* curr, int initialCapacity){
     curr->data = (int*)malloc(sizeof(int) * initialCapacity);
 }
 
-int main(int* argc, char* argv[]){
+void dyarr_push(DynamicArray* curr, int item){
+    // resize if needed
+    float loadFactor = (float)curr->length / curr->capacity;
+    if (loadFactor >= 0.5){
+        int* newData = (int*)malloc(2 * curr->capacity * sizeof(int));
+
+        for(int i = 0; i < curr->length; i++){
+            *(newData + i) = *(curr->data + i);
+        }
+
+        free(curr->data);
+
+        curr->data = newData;
+        curr->capacity = 2 * curr->capacity;
+    }
+    
+
+    *(curr->data + curr->length) = item;
+    curr->length = curr->length + 1;
+}
+
+int darr_get(DynamicArray* curr, int index){
+    if(index < 0 || index >= curr->length){
+        fprintf(stderr, "Index out of range.\n");
+        exit(1);
+    }
+
+    return *(curr->data + index); // (*(*curr).data)
+}
+
+int main(int argc, char* argv[]){
     DynamicArray w = {.data = NULL, .length = 0, .capacity = 0};
     initialize_array(&w, 3);
 
-    *(w.data) = 7; 
+    for (int i = 0; i < 10; i++){
+        dyarr_push(&w, 888);
+        printf("Length of array: %d, Capacity: %d\n", w.length, w.capacity);
+    }
+
+    for(int i = 0; i < w.length; i++){
+        printf("%d\n", darr_get(&w, i));
+    }
 
     printf("Dynamic array information: first element: %i,  length: %i, capacity: %i\n", *w.data, w.length, w.capacity);
 
