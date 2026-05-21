4da75c1815a0ea0b65945839be3389f01d24ad84
Renis Ndregjoni <ndregjonirenis@gmail.com>
2026-05-21 15:17:11 +0200

    Learning HTML

--- project/javascript/3 JS/write_javascript/main.js
+++ project/javascript/3 JS/write_javascript/main.js
@@ -3,29 +3,29 @@ var discount = 10;
 var total = price - (price * (discount / 100));
 console.log(total);
 
-// if (total > 25) {
-//     freeShipping();
-// 
-// }
-// 
-// var count = 10;
-// while (count > 0) {
-//     juggle();
-//     count = count -1;
-// }
-// 
-// var dog = {
-//     name : "Rover",
-//     weight: 35
-// };
-// 
-// 
-// if (dog.weight > 30) {
-//     alert("WOOF WOOF");
-// } else {
-//     alert("woof woof");
-// }
-// 
-// var circleRadius = 20;
-// var circleArea = Math.PI * (circleRadius * circleRadius);
-// console.log(circleArea);
+if (total > 25) {
+    freeShipping();
+
+}
+
+var count = 10;
+while (count > 0) {
+    juggle();
+    count = count -1;
+}
+
+var dog = {
+    name : "Rover",
+    weight: 35
+};
+
+
+if (dog.weight > 30) {
+    alert("WOOF WOOF");
+} else {
+    alert("woof woof");
+}
+
+var circleRadius = 20;
+var circleArea = Math.PI * (circleRadius * circleRadius);
+console.log(circleArea);
