e4e52b41f028afba36415fe784376d5255ef3ef2
Renis Ndregjoni <ndregjonirenis@gmail.com>
2026-05-25 14:59:50 +0200

    Learn JS

---
+++ project/programing_languages/javascript/2 JS/1. Basics/lesson_11.js
@@ -0,0 +1,13 @@
+let person = {
+    name: 'Renis', 
+    surname: 'Ndregjoni',
+    email: 'ndregjonirenis@gmail.com',
+    age: 21,
+}
+
+console.log(person.name);
+console.log(person.surname);
+console.log(person.email);
+console.log(person.age);
+
+console.log(person);
